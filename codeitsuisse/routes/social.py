e22d9e27877fabc8c48ea10966a0e89fc15290ce
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-09-26 19:26:25 +0800

    Update social.py

--- codeitsuisse/routes/social.py
+++ codeitsuisse/routes/social.py
@@ -1,6 +1,9 @@
 import logging
 import json
 import random
+import operator as op
+from functools import reduce
+
 
 from flask import request, jsonify;
 
@@ -14,12 +17,29 @@ def evaluateSOCIAL():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
     cases = data["tests"]
-    ans = []
+    ans = "{ \"answers\":{"
     for x in cases:
         y = calpos(cases[x]["seats"],cases[x]["people"],cases[x]["spaces"])
-        ans.append(y)
-    logging.info("My result :{}".format(data))
+        ans = ans + "\"" + x + "\": " + str(y) + ","
+    ans = ans[:-1]
+    ans = ans + "}}"
+    logging.info("My result :{}".format(ans))
     return jsonify(ans);
 
+
+def ncr(n, r):
+    r = min(r, n-r)
+    numer = reduce(op.mul, range(n, n-r, -1), 1)
+    denom = reduce(op.mul, range(1, r+1), 1)
+    return numer // denom  
+
 def calpos(seat,ppl,space):
-    return 1
+    numspace  = seat-ppl + 1
+    total = ncr(numspace,ppl)
+    i = 1
+    while space > i:
+        numspace = numspace - i + 1
+        for j in range(1,ppl):
+            total = total - ncr(numspace-j,ppl-j)
+        i = i + 1
+    return total
