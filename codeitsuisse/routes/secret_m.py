a9b6bab0a0cf1f30e6130612e454ac7cb6cb5e0f
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-09-26 09:02:36 +0800

    giof

---
+++ codeitsuisse/routes/secret_m.py
@@ -0,0 +1,23 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/sort', methods=['POST'])
+def evaluateSort():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    result = []
+    for x in data :
+        result.append(encrypt(x["n"],x["text"]))
+    logging.info("My result :{}".format(data))
+    return jsonify(result);
+
+
+def encrypt(n, text):
+    processed = ""
+    
