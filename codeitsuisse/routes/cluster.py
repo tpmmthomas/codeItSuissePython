a6d630ebd732d96dadf8976e62be26f319f92c0d
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-09-26 15:49:27 +0800

    Update cluster.py

--- codeitsuisse/routes/cluster.py
+++ codeitsuisse/routes/cluster.py
@@ -15,8 +15,52 @@ def evaluateCluster():
     logging.info("data sent for evaluation {}".format(data))
     rows = len(data)
     cols = len(data[0])
-
+    i = 0
+    j = 0
+    arr = [[0]*cols]*rows
+    result = 0
+    while i < rows:
+        while j < cols:
+            if data[i][j] == "1" and arr[i][j]=="0":
+                clusterarr(data,arr,i,j,rows,cols)
+                result = result+1
     logging.info("My result :{}".format(result))
     return json.dumps(result);
+
+def clusterarr(arr,data,i,j,rows,cols):
+    arr[i][j]=1
+    if i-1>=0 and j-1>=0 and data[i-1][j-1]=="0":
+        clusterarr(arr,data,i-1,j-1,rows,cols)
+    if i-1>=0 and j-1>=0 and data[i-1][j-1]=="1" and arr[i-1][j-1] == "0":
+        clusterarr(arr,data,i-1,j-1,rows,cols)
+    if i-1>=0  and data[i-1][j]=="0":
+        clusterarr(arr,data,i-1,j,rows,cols)
+    if i-1>=0  and data[i-1][j]=="1" and arr[i-1][j] == "0":
+        clusterarr(arr,data,i-1,j,rows,cols)
+    if i-1>=0 and j+1<cols and data[i-1][j+1]=="0":
+        clusterarr(arr,data,i-1,j+1,rows,cols)
+    if i-1>=0 and j+1<cols and data[i-1][j+1]=="1" and arr[i-1][j+1] == "0":
+        clusterarr(arr,data,i-1,j+1,rows,cols)
+    if j-1>=0  and data[i][j-1]=="0":
+        clusterarr(arr,data,i,j-1,rows,cols)
+    if j-1>=0  and data[i][j-1]=="1" and arr[i][j-1] == "0":
+        clusterarr(arr,data,i,j-1,rows,cols)
+    if j+1<cols  and data[i][j+1]=="0":
+        clusterarr(arr,data,i,j+1,rows,cols)
+    if j-1<cols  and data[i][j+1]=="1" and arr[i][j+1] == "0":
+        clusterarr(arr,data,i,j+1,rows,cols)
+    if i+1<rows and j-1>=0 and data[i+1][j-1]=="0":
+        clusterarr(arr,data,i+1,j-1,rows,cols)
+    if i+1<rows and j-1>=0 and data[i+1][j-1]=="1" and arr[i+1][j-1] == "0":
+        clusterarr(arr,data,i+1,j-1,rows,cols)
+    if i+1<rows  and data[i+1][j]=="0":
+        clusterarr(arr,data,i+1,j,rows,cols)
+    if i+1<rows  and data[i+1][j]=="1" and arr[i+1][j] == "0":
+        clusterarr(arr,data,i+1,j,rows,cols)
+    if i+1<rows and j+1<cols and data[i+1][j+1]=="0":
+        clusterarr(arr,data,i+1,j+1,rows,cols)
+    if i+1<rows and j+1<cols and data[i+1][j+1]=="1" and arr[i+1][j+1] == "0":
+        clusterarr(arr,data,i+1,j+1,rows,cols)
+    
 
 
