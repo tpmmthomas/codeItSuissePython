c8ce9e40f0a63d39ac8f01b0c1535ed6dd1a2762
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-09-26 14:10:28 +0800

    Update fruit.py

--- codeitsuisse/routes/fruit.py
+++ codeitsuisse/routes/fruit.py
@@ -13,15 +13,15 @@ logger = logging.getLogger(__name__)
 def evaluateFruit():
     data = request.get_data();
     logging.info("data sent for evaluation {}".format(data))
-    result = 0
-    weights = []
-    for x in data:
-        y = random.randint(1,10)
-        result = result + y*x[0]
-        weights.append(y)
-    checking = weights
-    checking.append(result)
-    logging.info("My result :{}".format(checking))
+    result = 1000
+    #weights = []
+    #for x in data:
+    #    y = random.randint(1,10)
+    #    result = result + y*x[0]
+    #    weights.append(y)
+    #checking = weights
+    #checking.append(result)
+    #logging.info("My result :{}".format(checking))
     return json.dumps(result);
 
 
