73101ce25a432fbb8ee5900ef88162d556f9489c
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-09-26 14:46:50 +0800

    Update contact.py

--- codeitsuisse/routes/contact.py
+++ codeitsuisse/routes/contact.py
@@ -13,15 +13,64 @@ logger = logging.getLogger(__name__)
 def evaluateContact():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
-    result = 20000
-    #weights = []
-    #for x in data:
-    #    y = random.randint(1,10)
-    #    result = result + y*x[0]
-    #    weights.append(y)
-    #checking = weights
-    #checking.append(result)
-    #logging.info("My result :{}".format(checking))
+    Clusters = data["cluster"]
+    Infected = data["infected"]
+    Origin = data["origin"]
+    result = []
+    current_path = [Infected["name"]]
+    RecurPath(Infected,Clusters,Origin,current_path,result)
+    logging.info("My result :{}".format(result))
     return json.dumps(result);
+
+def RecurPath(Infected,Clusters,Origin,current_path,result):
+    IsPath, IsNonSi = genomeCompare(Infected["genome"],Origin["gnome"])
+    if IsPath:
+        if IsNonSi: 
+            current_path[-1] = current_path[-1] + "*"
+        current_path.append(Origin["name"])
+        AddToResult(current_path,result)
+        current_path.pop(-1)
+    for contact in Clusters:
+        if contact in current_path:
+            continue
+        if contact+"*" in current_path:
+            continue
+        IsPath, IsNonSi =  genomeCompare(Infected["genome"],contact["gnome"])
+        if IsPath:
+            if IsNonSi: 
+                current_path[-1] = current_path[-1] + "*"
+            current_path.append(contact["name"])
+        RecurPath(contact,Clusters,Origin,current_path,result)
+        current_path.pop(-1)
+
+
+
+def AddToResult(current_path,result):
+    thepath = ""
+    for x in current_path:
+        thepath = thepath + x + " -> "
+    thepath = thepath[:-4]
+    result.append(thepath)
+
+
+def genomeCompare(x,y):
+    i = 0
+    numdiff = 0
+    numfirst = 0 
+    while i < len(x):
+        if x[i] != y[i]:
+            numdiff = numdiff + 1
+            if i == 0 or x[i-1] == "-":
+                numfirst = numfirst + 1
+        i = i + 1
+    if numdiff <= 2:
+        if numfirst > 1:
+            return True,True
+        else:
+            return True, False
+
+    return False,False
+
+
 
 
