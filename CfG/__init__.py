5a9d5548d6db421137bc525967621d06b7d753ca
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-10-09 09:25:15 +0800

    Update __init__.py

--- CfG/__init__.py
+++ CfG/__init__.py
@@ -1,18 +1,4 @@
 from flask import Flask;
 app = Flask(__name__)
-import codeitsuisse.routes.square
-import codeitsuisse.routes.sort
-import codeitsuisse.routes.secret_m
-import codeitsuisse.routes.salad
-import codeitsuisse.routes.geometry
-import codeitsuisse.routes.fruit
-import codeitsuisse.routes.contact
-import codeitsuisse.routes.cluster
-import codeitsuisse.routes.gmo
-import codeitsuisse.routes.social
-import codeitsuisse.routes.slsm
-import codeitsuisse.routes.inventory
-import codeitsuisse.routes.supermarket
-import codeitsuisse.routes.clean
-import codeitsuisse.routes.olympiad
-import codeitsuisse.routes.optimized
+import CfG.routes.square
+import CfG.routes.sort
